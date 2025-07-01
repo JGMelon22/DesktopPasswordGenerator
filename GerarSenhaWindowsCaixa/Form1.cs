1f94e03bec2a607b872b5d2eb818ba103bbd9dc7
JGMelon22 <jgvasconcellos22@gmail.com>
2025-06-30 21:26:25 -0300

    chore: allow only alpha numeric charaters

--- GerarSenhaWindowsCaixa/Form1.cs
+++ GerarSenhaWindowsCaixa/Form1.cs
@@ -1,55 +1,64 @@
-namespace GerarSenhaWindowsCaixa
+using System.Text.RegularExpressions;
+
+namespace GerarSenhaWindowsCaixa;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    [GeneratedRegex(@"[^a-zA-Z0-9\s]")]
+    private static partial Regex AlphaNumericRegex();
+
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-            textBox1.Clear();
-            button2.Enabled = false;
-        }
+        InitializeComponent();
+        textBox1.Clear();
+        button2.Enabled = false;
+    }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            textBox1.Clear();
-            button2.Enabled = false;
-        }
+    private void button2_Click(object sender, EventArgs e)
+    {
+        textBox1.Clear();
+        button2.Enabled = false;
+    }
 
-        private void button1_Click(object sender, EventArgs e)
+    private void button1_Click(object sender, EventArgs e)
+    {
+        PasswordGeneratorService passwordGeneratorService = new();
+        try
         {
-            PasswordGeneratorService passwordGeneratorService = new();
-            try
+            string nomePais = textBox1.Text.ToString();
+
+            if (!string.IsNullOrEmpty(nomePais) && !string.IsNullOrWhiteSpace(nomePais))
             {
-                string nomePais = textBox1.Text.ToString();
-
-                if (!string.IsNullOrEmpty(nomePais) && !string.IsNullOrWhiteSpace(nomePais))
-                {
-                    string senha = passwordGeneratorService.NovaSenha(nomePais);
-                    MessageBox.Show(senha, "Sua nova senha", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-
-                if (textBox1.Text.Length > 0)
-                {
-                    button2.Enabled = true;
-                }
+                string senha = passwordGeneratorService.NovaSenha(nomePais);
+                MessageBox.Show(senha, "Sua nova senha", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
 
-        private void textBox1_Leave(object sender, EventArgs e)
-        {
             if (textBox1.Text.Length > 0)
             {
                 button2.Enabled = true;
             }
         }
-
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        catch (Exception ex)
         {
-            e.Handled = e.KeyChar == (char)Keys.Space;
+            MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private void textBox1_Leave(object sender, EventArgs e)
+    {
+        if (textBox1.Text.Length > 0)
+        {
+            button2.Enabled = true;
+        }
+    }
+
+    private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        e.Handled = e.KeyChar == (char)Keys.Space;
+
+        if (AlphaNumericRegex().IsMatch(e.KeyChar.ToString()))
+        {
+            e.Handled = true;
         }
     }
 }
